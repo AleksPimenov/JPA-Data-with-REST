bbdab0d7cd3a9f1e47c30102c2e0e2567e80bf02
aleks pimenov <alekspimenov83@gmail.com>
2025-03-01 21:25:55 +0700

    Init

---
+++ src/main/java/Com/JPA_Data_with_REST/JpaDataWithRestApplication.java
@@ -0,0 +1,13 @@
+package Com.JPA_Data_with_REST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaDataWithRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaDataWithRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Com/JPA_Data_with_REST/JpaDataWithRestApplicationTests.java
@@ -0,0 +1,13 @@
+package Com.JPA_Data_with_REST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaDataWithRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
